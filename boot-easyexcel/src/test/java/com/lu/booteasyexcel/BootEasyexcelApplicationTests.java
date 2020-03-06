5c62e57926ae396bca71ae8f7bb2c61f54af83aa
bingyangmac <1016130309@qq.com>
2020-03-06 19:05:55 +0800

    boot-easypoi 1

---
+++ boot-easyexcel/src/test/java/com/lu/booteasyexcel/BootEasyexcelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lu.booteasyexcel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootEasyexcelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
